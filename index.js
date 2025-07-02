377995290075da61a4d861798ee5b6c87fcc3475
minijmann <minijmann@gmail.com>
2025-07-02 01:53:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ fastify.get('/clicks', async (req, reply) => {
   return { total: globalClicks };
 });
 
-fastify.listen({ port: process.env.PORT || 3000 }, err => {
+// 👇 Fix is here: use 0.0.0.0 for external access
+fastify.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' }, (err, address) => {
   if (err) throw err;
-  console.log('Server running');
+  console.log('Server running on ' + address);
 });
